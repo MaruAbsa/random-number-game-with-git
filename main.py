03ec59251dcd71d4357799e0482056baebac6ec3
MaruAbsa <umair.abbasi@ada.ac.uk>
2022-11-04 12:39:02 +0000

    Updated

--- main.py
+++ main.py
@@ -3,15 +3,22 @@ num = 0
 
 def generate_random_number():
   return random.randint(1,100)
-  
 
 
-def difference_from_answer(guess):
-  answer = generate_random_number()
+
+def difference_from_answer(guess,answer):
   if guess == answer:
     return "Correct"
   elif guess > answer:
     return "Guess is too high"
   else:
     return "Guess is too low"
-print(difference_from_answer(100))
+
+
+def make_a_guess():
+   user = int(input("Guess a number between 1 and 100 "))
+   random_number = generate_random_number()
+   return difference_from_answer(user,random_number)
+
+
+print(make_a_guess())
